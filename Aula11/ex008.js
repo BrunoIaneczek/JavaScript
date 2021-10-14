eecfe0c655709f02d74d9f5007b1a4e563478628
BrunoIaneczek / BrunoIaneczek <brunoamaralianeczek@gmailcom>
2021-10-13 22:08:03 -0300

    conteudo

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapssou a velocidade permitida. Multado`)
+}
+
+console.log(`Dirija sempre com segurança!`)
