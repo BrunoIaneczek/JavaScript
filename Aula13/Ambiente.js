b855447f199850bdf2460bd6077f0b07a65a0224
Bruno Amaral Ianeczek <87914395+BrunoIaneczek@users.noreply.github.com>
2021-11-07 09:47:03 -0300

    07_11_21

---
+++ Aula13/Ambiente.js
@@ -0,0 +1,16 @@
+/* ESTRTURA DE REPETIÇÃO COM TESTE LÓGICO NO ÍNICIO */
+
+/*var c = 1
+while (c <= 6){
+    console.log(`${c} tudo bem?`)
+    c++ /* c++ e a mesma coisa que c +=1
+}*/
+
+
+/* ESTRTURA DE REPETIÇÃO COM TESTE LÓGICO NO FINAL */
+
+var c = 1
+do {
+    console.log(`tudo bem?`)
+    c++
+} while(c <= 6) 
