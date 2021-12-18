0a4be5718a772009eafe418f911f572122348e9a
BrunoIaneczek / BrunoIaneczek <brunoamaralianeczek@gmailcom>
2021-12-17 21:12:09 -0300

    17_12_21

---
+++ Aula15/Ambiente.js
@@ -0,0 +1,14 @@
+let num = [1,23,12,4,67,14,5]
+/*num.sort()  ordena os elementos*/
+/*num[3] = 6
+num.push(13) /* acrescenta item */
+ /* conta quantos elementos tem o vetor*/
+let c = 0
+let itens = num.length
+while (c < itens){
+    console.log(num[c])
+    c++
+}
+/*console.log(`nosso vetor é o ${num}`)
+console.log(`Meu vetor tem ${num.length}`)*/
+ 
