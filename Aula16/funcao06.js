afd53fcc08d039e96fae4ba3aa9ca0cf9c93b92c
Bruno Amaral Ianeczek <87914395+BrunoIaneczek@users.noreply.github.com>
2021-12-22 22:42:50 -0300

    22_12_21

---
+++ Aula16/funcao06.js
@@ -0,0 +1,9 @@
+let numeros = []
+function adicionar(n){
+     numeros.push(n)
+     return numeros  
+
+}
+
+console.log(adicionar(4))
+console.log(adicionar(5))
--- Exercício_final/exerciciofinal.js
+++ Exercício_final/exerciciofinal.js
@@ -1,20 +1,21 @@
+let num = document.getElementById('n')
+let tabela = document.getElementById('tabela')
+let res = document.getElementById('res')
+let valores = [2,3,4]
+
 function limpar(){
     document.getElementById('n').value = ''
 }
+ 
 function analisar(){
-    let num = document.getElementById('n').value
-    let tabela = document.getElementById('tabela')
-    let resumo = document.getElementById('res')
-    let numeros = []
-    
-    if (num < 1 || num > 100 || num.length == 0){
+   
+    if (num.value< 1 || num.value> 100 || num.value.length == 0){
         alert('valor inválido')
     }else{
         let item = document.createElement('option')
-        item.text = `Valor ${num} adicionado`
-        tabela.appendChild(item)
-        resumo.innerHTML=(num)
-
+        item.text = `Valor ${num.value} adicionado`
+        tabela.appendChild(item)        
     }
 
-} 
+
+}res.innerHTML = `${valores.length}`
